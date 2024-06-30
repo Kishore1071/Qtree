1b29d0251a69b531246df029c2038d14099ef0d8
Kishore1071 <kishore240398@gmail.com>
2024-06-30 18:50:15 +0530

    Updated

---
+++ JavaScript/Basics/3__Operators.js
@@ -0,0 +1,26 @@
+// Arithmetic Operators
+// Assignment Operators
+// Comparison Operators
+// Logical Operators
+
+
+`Arithmetic Operators`
+
+    // +, -, *, /, %, **, ++, --
+
+
+`Assignment Operators`
+
+    // =
+
+
+`Comparison Operators`
+
+    // ==, ===, !=, !==, >, <, >=, <=, ?:
+
+
+`Logical Operators`
+
+    // &&, ||, !
+
+
